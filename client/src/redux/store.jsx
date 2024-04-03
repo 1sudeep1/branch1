3e8053c2683226debdd6410e0c55c637ffedd18c
Sudeep Tharu <sudeep.tharu32@gmail.com>
2024-04-03 20:50:50 +0545

    redux persist setup done

--- client/src/redux/store.jsx
+++ client/src/redux/store.jsx
@@ -1,7 +1,20 @@
 import { configureStore} from '@reduxjs/toolkit'
 import userReducer from './reducerSlices/userSlice'
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
 import logger from 'redux-logger'
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, userReducer)
+
 export const store = configureStore({
-  reducer: userReducer,
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== 'production',
   middleware: () => [logger],
 })
+
+export const persistor = persistStore(store)
